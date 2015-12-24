daa8897af1a40f16f7bd0015333107562735ff9c
airect <sunxiaojiao001@gmail.com>
2015-12-24 16:16:36 +0800

    init

---
+++ router.js
@@ -0,0 +1,30 @@
+/**
+* router
+*/
+var express = reuire('express');
+var app = express();
+
+//关于我们
+app.get('/about', function(req, res) {
+	res.
+});
+
+//数据展示页
+app.get('/', function(req, res) {
+
+});
+
+//展示支出收入条目
+app.get('/', function(req, res) {
+
+});
+
+//添加收入支出页面
+
+
+//添加收入支出api
+
+//登陆
+//注册
+//找回密码
+
---
+++ setting.js
@@ -0,0 +1,9 @@
+module.exports = {
+	db: {
+		host: '',
+		user: 'root',
+		pwd : 'root',
+		port: '3306',
+		dbname: ''
+	}
+};

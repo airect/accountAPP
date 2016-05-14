c72831aa958701e404f4d961178c2def06071fb6
ariect <sunxiaojiao001@gmail.com>
2016-05-14 20:33:06 +0800

    angualr 大体框架结构

---
+++ public/angular/app.js
@@ -0,0 +1,21 @@
+/*
+ * angular 首页入口文件
+ */
+angular.module('account', [
+    'ui.router'
+    ,'account.user'
+
+]).run(function() {
+
+})
+.config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        // 配置默认路由跳转
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('index', {
+                url: '/',
+                templateUrl: 'public/angular/tpl/index/index.html'
+            });
+    }
+]);
---
+++ public/angular/app_user.js
@@ -0,0 +1,50 @@
+/**
+ * angular 用户中心入口
+ */
+
+angular.module('account.user', [
+    'ui.router'
+])
+.config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('user', {
+                absolute: true,
+                url: '/user',
+                templateUrl: 'public/angular/tpl/user/index.html'
+            })
+            /* 用户中心 */
+            .state('user.index', {
+                url: '',
+                views: {
+                    'add-btn': {
+                        templateUrl: 'public/angular/tpl/user/index/add_btn.html',
+                    },
+                    'trend': {
+                        templateUrl: 'public/angular/tpl/user/index/trend.html'
+                    },
+                    'checklist': {
+                        templateUrl: 'public.angular/tpl/user/index/check_list.html'
+                    }
+                }
+            })
+            /* 用户设置 */
+            .state('user.setting', {
+                url: '/setting',
+                templateUrl: 'public/angular/tpl/user/setting.html'
+            })
+            .state('user.setting.profile', {
+                url: '',
+                templateUrl: 'public/angular/tpl/user/setting/profile.html'
+            })
+            .state('user.setting.avatar', {
+                url: '/avatar',
+                templateUrl: 'public/angular/tpl/user/setting/avatar.html'
+            })
+            .state('user.setting.addtype', {
+                url: '/addtype',
+                templateUrl: 'public/angular/tpl/user/setting/addtype.html'
+            });
+}]);
+
